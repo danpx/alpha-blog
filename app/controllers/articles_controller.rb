7985aa89ed2958a34d10f4c541f77b7ffb5ce81e
DPisek <daniel.pisek@gmail.com>
2019-10-30 10:47:12 +0100

    ready to prod.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,51 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   def new
     @article = Article.new
   end
 
+  def index
+    @articles = Article.all
+  end
+
+  def edit
+  end
+
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article was sucessfuly created."
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article was sucessfuly updated."
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article was sucessfuly deleted."
+    redirect_to articles_path
+  end
+
+  def show
   end
 
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:title, :description)
   end
